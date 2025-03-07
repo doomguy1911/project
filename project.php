f59b5895e0d439d654dbf2de771d5443a8749e1d
Gregory <gregorynin2007@.com>
2025-03-07 19:15:49 -0400

    añadir funcionalidad

---
+++ project.php
@@ -0,0 +1,16 @@
+<?php
+// solicitar el nombre del usuario
+echo "hola, cuál es tu nombre?\n";
+$nombre = trim(fgets(STDIN));
+
+// solicitar el año de nacimiento
+echo "hola, $nombre. en qué año naciste?\n";
+$año_nacimiento = trim(fgets(STDIN));
+
+// Calcular la edad actual
+$año_actual = date("Y");
+$edad = $año_actual - $año_nacimiento;
+
+// Mostrar el resultado
+echo "tienes aproximadamente $edad años.\n";
+?>
